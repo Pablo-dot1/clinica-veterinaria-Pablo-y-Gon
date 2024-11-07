377ed270d86e03679b16d49e6ab5fadea37ca9af
Pablo-dot1 <pablo.huidobro.garcia@gmail.com>
2024-11-07 20:30:32 +0100

    Ajuste database uvicorn

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,24 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from routes import router  # Asegúrate de que 'routes.py' está en la misma carpeta
-from database import engine  # Importa tu engine de base de datos
-from models_db import Base  # Cambié la importación para que sea desde models_db
 
-# Crea las tablas en la base de datos
-Base.metadata.create_all(bind=engine)
-
+# Crea una instancia de FastAPI
 app = FastAPI(
     title="Servidor de datos de la clínica veterinaria",
-    description="""Gestión de datos de clientes, mascotas, citas y tratamientos.""",
+    description="Gestión de datos de clientes, mascotas, citas y tratamientos.",
     version="0.1.0",
 )
 
+# Definir el modelo para el formulario
 class FormData(BaseModel):
     date: str
     description: str
     option: str
     amount: float
 
+# Endpoint para recibir un formulario
 @app.post("/envio/")
 async def submit_form(data: FormData):
     return {"message": "Formulario recibido", "data": data}
