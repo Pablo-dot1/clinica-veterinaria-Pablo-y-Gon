34ae2c60949a2894173dfc3b6568d229e069cbf1
Pablo-dot1 <pablo.huidobro.garcia@gmail.com>
2024-11-07 08:21:59 +0100

    modificación server.py

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,51 +1,17 @@
-import shutil
-import io
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-import pandas as pd
-from typing import List
-from pydantic import BaseModel as PydanticBaseModel, Field
 from routes import router  # Asegúrate de que 'routes.py' está en la misma carpeta
+from database import engine  # Importa tu engine de base de datos
+from models_db import Base  # Cambié la importación para que sea desde models_db
 
-class BaseModel(PydanticBaseModel):
-    class Config:
-        arbitrary_types_allowed = True
-
-class Contrato(BaseModel):
-    fecha: str
-    centro_seccion: str
-    nreg: str
-    nexp: str
-    objeto: str
-    tipo: str
-    procedimiento: str
-    numlicit: str
-    numinvitcurs: str
-    proc_adjud: str
-    presupuesto_con_iva: str
-    valor_estimado: str
-    importe_adj_con_iva: str
-    adjuducatario: str
-    fecha_formalizacion: str
-    I_G: str
-
-class ListadoContratos(BaseModel):
-    contratos: List[Contrato] = Field(default_factory=list)
+# Crea las tablas en la base de datos
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-    title="Servidor de datos",
-    description="""Servimos datos de contratos, clientes, mascotas, citas y tratamientos.""",
+    title="Servidor de datos de la clínica veterinaria",
+    description="""Gestión de datos de clientes, mascotas, citas y tratamientos.""",
     version="0.1.0",
 )
-
-@app.get("/retrieve_data/")
-def retrieve_data():
-    todosmisdatos = pd.read_csv('./contratos_inscritos_simplificado_2023.csv', sep=';')
-    todosmisdatos = todosmisdatos.fillna(0)
-    todosmisdatosdict = todosmisdatos.to_dict(orient='records')
-    listado = ListadoContratos()
-    listado.contratos = todosmisdatosdict
-    return listado
 
 class FormData(BaseModel):
     date: str
